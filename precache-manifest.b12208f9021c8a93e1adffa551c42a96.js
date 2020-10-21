bc359b0091b180cd0703862418f1dda4e99f23ee
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2020-10-21 15:14:01 +0900

    Updates

---
+++ precache-manifest.b12208f9021c8a93e1adffa551c42a96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6dbb5112751614c045b8ade50489ba1",
+    "url": "/dvorak-typing/index.html"
+  },
+  {
+    "revision": "0780821b2af5d2a1a042",
+    "url": "/dvorak-typing/static/css/2.d7659245.chunk.css"
+  },
+  {
+    "revision": "9ef310e916334e826ca1",
+    "url": "/dvorak-typing/static/css/main.7a14a840.chunk.css"
+  },
+  {
+    "revision": "0780821b2af5d2a1a042",
+    "url": "/dvorak-typing/static/js/2.cef847cf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/dvorak-typing/static/js/2.cef847cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ef310e916334e826ca1",
+    "url": "/dvorak-typing/static/js/main.2c90da5d.chunk.js"
+  },
+  {
+    "revision": "a5d28556af52a8b333d4",
+    "url": "/dvorak-typing/static/js/runtime-main.d216f75e.js"
+  }
+]);
