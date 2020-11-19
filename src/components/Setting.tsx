a2e2d53e2984b3501a735625bdc65950bdd55b18
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2020-11-20 01:39:03 +0900

    import文の整理&Boxコンポーネント追加

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import Button from '@material-ui/core/Button';
-import InputLabel from '@material-ui/core/InputLabel';
-import Input from '@material-ui/core/Input';
+import {
+  Box,
+  Button,
+  InputLabel,
+  Input,
+  FormControl,
+  InputAdornment,
+  Container,
+} from '@material-ui/core';
 import AccountBox from '@material-ui/icons/AccountBox';
 import CloudIcon from '@material-ui/icons/Cloud';
-import FormControl from '@material-ui/core/FormControl';
-import { InputAdornment } from '@material-ui/core';
-import Container from '@material-ui/core/Container';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme: Theme) => {
@@ -59,47 +62,53 @@ const Setting: React.FC = () => {
 
   return (
     <Container maxWidth='lg'>
-      <h1>Setting</h1>
-      <FormControl fullWidth>
-        <InputLabel htmlFor="input-with-icon-adornment">username</InputLabel>
-        <Input
-          value={username}
-          onChange={(e) => handleChange(e, setUsername)}
-          startAdornment={
-            <InputAdornment position="start">
-              <AccountBox />
-            </InputAdornment>
-          }
-        />
-      </FormControl>
-      <FormControl fullWidth>
-        <InputLabel htmlFor="input-with-icon-adornment">server-url</InputLabel>
-        <Input
-          value={rankingServerUrl}
-          onChange={(e) => handleChange(e, setRankingServerUrl)}
-          startAdornment={
-            <InputAdornment position="start">
-              <CloudIcon />
-            </InputAdornment>
-          }
-        />
-      </FormControl>
-      <div className={classes.button}>
-      <Button
-        size='small'
-        variant="outlined"
-        onClick={saveSettings}
+      <Box
+        display='flex'
+        flexDirection='column'
+        height='100vh'
       >
-        Save
-      </Button>
-      <Button
-        size='small'
-        variant="outlined"
-        onClick={clearSettings}
-      >
-        Clear
-      </Button>
-      </div>
+        <h1>Setting</h1>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="input-with-icon-adornment">username</InputLabel>
+          <Input
+            value={username}
+            onChange={(e) => handleChange(e, setUsername)}
+            startAdornment={
+              <InputAdornment position="start">
+                <AccountBox />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="input-with-icon-adornment">server-url</InputLabel>
+          <Input
+            value={rankingServerUrl}
+            onChange={(e) => handleChange(e, setRankingServerUrl)}
+            startAdornment={
+              <InputAdornment position="start">
+                <CloudIcon />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <div className={classes.button}>
+        <Button
+          size='small'
+          variant="outlined"
+          onClick={saveSettings}
+        >
+          Save
+        </Button>
+        <Button
+          size='small'
+          variant="outlined"
+          onClick={clearSettings}
+        >
+          Clear
+        </Button>
+        </div>
+      </Box>
     </Container>
   )
 }
